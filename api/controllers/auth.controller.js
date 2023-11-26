a128fd3cce5112eb5725d50197e97946742907b5
Lyes <alyes.its@gmail.com>
2023-11-26 21:47:40 +0100

    sign-in-api-route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,9 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const signup = async (req, res, next) => {
 
@@ -15,6 +19,22 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error)
 
+  }
+}
+export const signin = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ username });
+    if (!validUser) return next(errorHandler(404, 'User Not Found'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Incorrect Password'));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...otherValuesWithoutPassword } = validUser._doc; //destructuring the password from the response object because we don't want to return hashed password in the response
+    res.cookie('access_token', token, { httpOnly: true, expires: new Date(Date.now() + 24) }).status(200).json(otherValuesWithoutPassword);
+
+  } catch (error) {
+    next(error)
   }
 }
 
